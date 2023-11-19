bd1810f4c96c8e207c42bc90d6c9e1c49d46e382
NEERAJ DHAKAD <dhakadneeraj786@gmail.com>
2023-11-19 22:56:33 +0530

    added-user-and-video-model

---
+++ src/models/user.model.js
@@ -0,0 +1,97 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // For searching (optimised)
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudnary url service to store images like aws
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshTokens: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+//convert password field to a token if password field is modified 
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+//used when LOGIN : compare entered password with the encrypted password
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password,this.password)
+}
+
+//Generate Access Token using JWT
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign(
+        {   
+            //Payload or data to be encrypted
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullName:this.fullName
+        },
+        //SECRET KEY
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+//Generate Refresh Token using JWT
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {   
+            //Payload or data to be encrypted
+            _id:this._id,
+        },
+        //SECRET KEY
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema);
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -8,7 +8,10 @@ const asyncHandler = (requestHandler) =>{
 
 export {asyncHandler}
 
-
+// This code defines a function asyncHandler that takes a requestHandler as an argument.
+// The asyncHandler function returns an anonymous function that handles the request and response, as well as the next middleware function.
+// It ensures that the requestHandler is executed within a Promise context and catches any errors that occur.
+// The asyncHandler function is exported to be used elsewhere.
 
 //Higher order funtion because it takes a function as an argument
 // const asyncHandler = (fn) => async(req,res,next) => {
